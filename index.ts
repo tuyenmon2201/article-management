1939ba8071aa1f0379f655b89cd1e102bb470049
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-28 16:27:58 +0700

    Connect database

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import express, { Express, Request, Response } from "express";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app: Express = express();
 const port: number = 3003;
 
+import { connectDatabase } from "./config/database";
+connectDatabase();
+
+import Article from "./models/article.model";
+
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+    const articles = await Article.find({
+        deleted: false
+    });
+
     res.json({
-        articles: []
+        articles: articles
     });
 });
 
