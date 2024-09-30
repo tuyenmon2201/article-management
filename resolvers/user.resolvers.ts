a9b026f777f5a05b1ad0162199b44220b33ee8de
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-30 22:34:48 +0700

    Feature login

--- resolvers/user.resolvers.ts
+++ resolvers/user.resolvers.ts
@@ -40,6 +40,41 @@ export const resolversUser = {
                 message: "Đăng ký thành công!",
                 token: token
             };
+        },
+
+        login: async (_, args) => {
+            const { user } = args;
+
+            const email: string = user.email;
+            const password: string = user.password;
+
+            const existUser = await User.findOne({
+                email: email,
+                deleted: false
+            });
+
+            if(!existUser){
+                return {
+                    code: 400,
+                    message: "Email không tồn tại!"
+                };
+            }
+
+            if(md5(password) != md5(existUser.password)){
+                return {
+                    code: 400,
+                    message: "Sai mật khẩu!"
+                }
+            }
+
+            return {
+                id: existUser.id,
+                fullName: existUser.fullName,
+                email: existUser.email,
+                code: 200,
+                message: "Đăng nhập thành công!",
+                token: user.token
+            }
         }
     }
 };
--- typeDefs/user.typeDefs.ts
+++ typeDefs/user.typeDefs.ts
@@ -10,13 +10,19 @@ export const typeDefsUser = gql`
         message: String
     }
 
-    input UserInput {
+    input RegiterInput {
         fullName: String,
         email: String,
         password: String
     }
 
+    input LoginInput {
+        email: String,
+        password: String
+    }
+
     type Mutation {
-        register(user: UserInput): User
+        register(user: RegiterInput): User,
+        login(user: LoginInput): User
     }
 `;
